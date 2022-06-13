e096f393ea1ff08ba3d8a03799d3452486604152
sebastiantuyu <alextpineiro@gmail.com>
2022-06-12 21:46:17 -0500

    Initial Commit

---
+++ Gamify/GamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamifyApp.swift
+//  Gamify
+//
+//  Created by Sebastian on 12/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
